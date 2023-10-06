baf0c0408f768c9c360309da182273ea7185ecc5
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-06 09:41:02 -0300

    aula-05

---
+++ exercicio.js
@@ -0,0 +1,10 @@
+let frutas = ["morango", "banana", "mamão", "pera"];
+
+if(frutas.includes("abacaxi")){
+    console.log("tem abacaxi")
+}else if (frutas.includes("pera")){
+    console.log("tem pera bro")
+}
+else{
+    console.log("nem pera nem abacaxi")
+}
---
+++ switch.js
@@ -0,0 +1,12 @@
+let teste = 2;
+switch(teste){
+    case 1:
+        console.log("foi de primeira");
+        break
+case 2:
+console.log("foi de segunda vez");
+    break;
+default:
+console.log("nao foi nem de primeira nem de segunda")
+    break;
+}
