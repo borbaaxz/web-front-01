60ab75a9650b6e08419628e26dd7e58fd044ca64
gabriel borba <129815052+borbaaxz@users.noreply.github.com>
2023-10-27 09:56:51 -0300

    dsad

--- dom.js
+++ dom.js
@@ -1,7 +1,3 @@
-function teste(arg1, arg2){
-    return arg1 + arg2
+function cliqueAqui(){
+    alert("olá");
 }
-function imprimeNome(nome){
-    console.log('meu nome é {nome}');
-}
-console.log(teste(5,4))
